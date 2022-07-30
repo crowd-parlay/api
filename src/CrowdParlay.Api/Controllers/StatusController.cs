bebc0e8e0ebb6449afa3bcd01c1abf4264030ffb
undrcrxwn <undrcrxwn@gmail.com>
2022-07-30 20:07:47 +0300

    Cleanup

---
+++ src/CrowdParlay.Api/Controllers/StatusController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrowdParlay.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StatusController : Controller
+{
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
